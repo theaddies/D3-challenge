974ae54cb27e00645f7e9345b72e003f222e8ebc
Andrew Addie <theaddies@neo.rr.com>
2020-08-27 18:20:11 -0400

    a simple graph with blue dots

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,90 @@
 // @TODO: YOUR CODE HERE!
+
+var svgWidth = 1200;
+var svgHeight = 660;
+console.log("hi there");
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+// append svg and group
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//  Import the data and start the promise
+
+d3.csv("./assets/data/data.csv").then(function (healthData) {
+    console.log("healthData", healthData);
+    console.log([healthData]);
+
+        //tidy data to make sure it is all numbers
+
+    healthData.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.povertyMoe = +data.povertyMo
+        data.age = +data.age
+        data.ageMoe = +data.ageMoe
+        data.income = +data.income
+        data.incomeMoe = +data.incomeMoe
+        data.healthcare = +data.healthcare
+        data.healthcareLow = +data.healthcareLow
+        data.healthcareHigh = +data.healthcareHigh
+        data.obesity = +data.obesity
+        data.obesityLow = +data.obesityLow
+        data.obesityHigh = +data.obesityHigh
+        data.smokes = +data.smokes
+        data.smokesLow = +data.smokesLow
+        data.smokesHigh = +data.smokesHigh
+
+    })
+
+    // create x and y scales
+    var xScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.poverty)])
+        .range([0, width]);
+
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.smokes)])
+        .range([height, 0]);
+
+    // Create axis functions
+
+    var bottomAxis = d3.axisBottom(xScale);
+
+    var leftAxis = d3.axisLeft(yScale);
+
+    //append axes to the chart
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    //create circles for data
+
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.poverty))
+    .attr("cy", d => yScale(d.smokes))
+    .attr("r", "15")
+    .attr("fill", "LightSkyBlue")
+    
+
+
+
+})
